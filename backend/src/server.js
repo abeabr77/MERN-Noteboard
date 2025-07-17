03014369c38c14125a2f67836f38e3b51f472922
abeabr77 <abenezeradev@outlook.com>
2025-07-17 02:18:24 -0700

    routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,9 @@
 import express from "express"
+import notesRoutes from"./routes/notesRoutes.js";
 const app = express()
 
 app.listen(5002, () => {
     console.log("Server started on PORT: 5002");
 });
 
-app.get("/api/notes", (req, res) => {
-    res.send("you got 5 notes");
-});
+app.use("/api/notes", notesRoutes)
