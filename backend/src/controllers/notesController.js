2dc297e4e6563ba3a9b48b481801067ae838565d
abeabr77 <abenezeradev@outlook.com>
2025-07-26 03:01:35 -0700

    controllers done

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,15 +1,64 @@
-export const getAllNotes = (req, res) => {
-    res.status(200).send("you just fetched the notes");
+import Note from "../models/Note.js"
+
+export async function getAllNotes(req, res) {
+    try {
+        const notes = await Note.find().sort({ createdAt: -1}); //shows newest note first
+        res.status(200).json(notes)
+    } catch (error) {
+        console.error("Error");
+        res.status(500).json({message: "server error"})
+    }
 };
 
-export const createNote = (req, res) => {
-    res.status(201).json({ message: "Note created successfully"});
+export async function getNoteById(req, res) {
+    try{
+        const note = await Note.findById(req.params.id);
+        if (!note) return res.status(404).json({message: "note not found"})
+        res.json(note);
+    } catch (error) {
+        console,error("error");
+        res.status(500).json({message: "server errpr"});
+    }
+}
+
+export async function createNote(req, res) {
+    try {
+        const {title, content} = req.body
+        const Note = new Note({title, content})
+
+        const savedNote = await Note.save()
+        res.status(201).json({message:"Note created succesfully"})
+    } catch (error) {
+        console.error("Error");
+        res.status(500).json({ message:"server error"});
+    }
 };
 
-export const updateNote = (req, res) => {
-    res.status(200).json({ message: "Note updated successfully"});
+export async function updateNote(req, res){
+    try{
+        const {title, content} = req.body;
+        await Note.findByIdAndUpdate(req.params.id,
+            {title,content},
+            {
+                new: true,
+            }
+        );
+
+        if (!updatedNote) return res.status(404).json({message:"Note not found"})
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        console.error("Error in createNote controller", error);
+        res.status(500).json({message: "Internal server error"});
+    }
 };
 
-export const deleteNote = (req, res) => {
-    res.status(200).json({ message: "Note deleted successfully"});
+export async function deleteNote(req, res){
+    try{
+        const deletedNote = await Note.findByIdAndDelete(req.params.id)
+        if(!deletedNote) return res.status(404).json({message: "note not found"});
+        res.status(200).json({message:"Note deleted successfully"});
+    } catch (error) {
+        console.error("Error in deleteNote controller", error);
+        res.status(500).json({message:"server error"});
+    }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,8 @@ const app = express()
 
 connectDB();
 
+app.use(express.json())
+
 app.listen(5002, () => {
     console.log("Server started on PORT: 5002");
 });
@@ -16,4 +18,3 @@ app.use("/api/notes", notesRoutes)
 
 
 
-//mongodb+srv://abenezeradev:79LsXQyzfYPFK5nV@cluster0.z982czi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
